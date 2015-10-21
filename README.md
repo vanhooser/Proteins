b80c2ddaaca949a463518af9214b39d0927fd100
Shelby Vanhooser <shelbyvanhooser@Shelbys-MacBook-Pro-2.local>
2015-10-20 21:27:01 -0700

    Fully operational, print statements up and running

--- README.md
+++ README.md
@@ -3,32 +3,109 @@ _"But what protein structure **defines** a mouse?"_
 
 ![](https://upload.wikimedia.org/wikipedia/commons/6/60/Myoglobin.png)
 
-Find the dataset used [here](http://archive.ics.uci.edu/ml/datasets/Mice+Protein+Expression) and the starting code for SKLearn [here](http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVR.html)
+Find the dataset used [here](http://archive.ics.uci.edu/ml/datasets/Mice+Protein+Expression) and the starting code for SKLearn [here](http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html)
 
 
 _Requirements_ 
 
 * Python 2.7.x
-* [SKLearn](http://scikit-learn.org/stable/install.html)
+* [SKLearn](http://scikit-learn.org/stable/install.html) SVC
 * [Numpy](http://www.scipy.org/Installing_SciPy) _"Shouldn't you already have that installed?"_
-* For the love of god, an OS X machine
+* You're running OS X, right?
 
 **Special thanks to Higuera C, Gardiner KJ and Cios KJ for their data collection and work as well as the UCI Machine Learning Repository**
 
 ##Quickstart
 
 **~/ usr$ python proteins.py**
-> Still in development!
+>----------Kernel :  linear ----------
+>
+>
+>Genotype Analysis : 
+>
+> Correct : 154.00 (95.06 %)  
+> Incorrect : 8.00 (4.94 %)  
+> False Positive : 3.00 (1.85 %)  
+> False Negative : 5.00 (3.09 %)  
+>
+>
+> Treatment Analysis : 
+>
+> Correct : 121.00 (74.69 %)  
+> Incorrect : 41.00 (25.31 %)  
+> False Positive : 20.00 (12.35 %)  
+> False Negative : 21.00 (12.96 %)  
+>
+>
+> Behavior Analysis : 
+>
+> Correct : 162.00 (100.00 %)  
+> Incorrect : 0.00 (0.00 %)  
+> False Positive : 0.00 (0.00 %)  
+> False Negative : 0.00 (0.00 %)  
+>
+> ----------Kernel :  sigmoid ----------
+>
+>
+> Genotype Analysis : 
+>
+> Correct : 88.00 (54.32 %)  
+> Incorrect : 74.00 (45.68 %)  
+> False Positive : 0.00 (0.00 %)  
+> False Negative : 74.00 (45.68 %)  
+>
+>
+> Treatment Analysis : 
+>
+> Correct : 83.00 (51.23 %)  
+> Incorrect : 79.00 (48.77 %)  
+> False Positive : 0.00 (0.00 %)  
+> False Negative : 79.00 (48.77 %)  
+>
+>
+> Behavior Analysis : 
+>
+> Correct : 82.00 (50.62 %)  
+> Incorrect : 80.00 (49.38 %)  
+> False Positive : 80.00 (49.38 %)  
+> False Negative : 0.00 (0.00 %)  
+>
+> ----------Kernel :  rbf ----------
+>
+>
+> Genotype Analysis : 
+>
+> Correct : 123.00 (75.93 %)  
+> Incorrect : 39.00 (24.07 %)  
+> False Positive : 10.00 (6.17 %)  
+> False Negative : 29.00 (17.90 %)  
+>
+>
+> Treatment Analysis : 
+>
+> Correct : 98.00 (60.49 %)  
+> Incorrect : 64.00 (39.51 %)  
+> False Positive : 18.00 (11.11 %)  
+> False Negative : 46.00 (28.40 %)  
+>
+>
+> Behavior Analysis : 
+>
+> Correct : 162.00 (100.00 %)  
+> Incorrect : 0.00 (0.00 %)  
+> False Positive : 0.00 (0.00 %)  
+> False Negative : 0.00 (0.00 %)  
+
+_**Sweet mother of Turing, does that say 100%?**_
 
 
 ##Overview
+This dataset is a very interesting one as it has direct and predictive possibilities across a number of organisms, not just mice with the particular genotypes/behaviors/treatments sought by Higuera, Gardiner, and Cios.  The simple floating-point measurement of the concentrations of the proteins of interest make for easy insertion into a training/validation set, and the binary nature of the three output class types make it simple to implement SVC machines here with whatever kernel our hearts so desire.
 
+I've adapted this implementation slightly from my previous example, [Fire](https://github.com/thepropterhoc/Fire), to include a more agnostic view of the kernel functions used by the SVM, allowing us to dig a little deeper into which classifies the three groups best. 
 
 ##Differences
-
+I've interpreted a sample from a mouse missing a particular protein measurement to have a value of _0.0_ for that protein, but it may be safer to remove these samples from the set instead.  This could be looked into next, but I've kept them here with this _0.0_ for the sake of keeping the training/validation sizes of the sets reasonable. 
 
 ##Results
-
-
-
-
+Wow.  Yes in fact, a linear kernel has near-perfect prediction abilities for each of the three groups of classes sought (Genotype and Behavior).  
--- proteins.py
+++ proteins.py
@@ -7,6 +7,14 @@ import random
 
 
 test_size = 0.15
+kernels = ['linear', 'sigmoid', 'rbf']
+degree=3
+gamma=0.0
+coef0=0.0
+tol=0.001
+C=1.0
+
+
 genotypes, treatments, behaviors = [], [], []
 inputs = []
 outputGenotypes, outputTreatments, outputBehaviors = [], [], []
@@ -35,27 +43,66 @@ with open('./proteins.csv', 'r') as csvfile:
 
 genotype_train, genotype_test, genotypeOutput_train, genotypeOutput_test = cross_validation.train_test_split(inputs, outputGenotypes, test_size=test_size, random_state=0)
 treatment_train, treatment_test, treatmentOutput_train, treatmentOutput_test = cross_validation.train_test_split(inputs, outputTreatments, test_size=test_size, random_state=0)
-behavior_train, behavior_test, behaviorOutput_train, behaviourOutput_test = cross_validation.train_test_split(inputs, outputBehaviors, test_size=test_size, random_state=0)
+behavior_train, behavior_test, behaviorOutput_train, behaviorOutput_test = cross_validation.train_test_split(inputs, outputBehaviors, test_size=test_size, random_state=0)
 
-genotypePredictor = svm.SVC().fit(genotype_train, genotypeOutput_train)
-treatmentPredictor = svm.SVC().fit(treatment_train, treatmentOutput_train)
-behaviorPredictor = svm.SVC().fit(behavior_train, behaviorOutput_train)
 
-genotypeCorrect, genotypeIncorrect , genotypeFalsePositive, genotypeFalseNegative, totalGenotype = 0,0,0,0,0
-for x, y in zip(genotype_test, genotypeOutput_test):
-	predicted = genotypePredictor.predict(x)[0]
-	if predicted == y:
-		genotypeCorrect += 1
-	else:
-		genotypeIncorrect += 1
-		if predicted == 1:
-			genotypeFalsePositive += 1
+
+for kernel in kernels:
+	print '----------Kernel : ', kernel, '----------\n'
+	genotypePredictor = svm.SVC(kernel=kernel, degree=degree, gamma=gamma, coef0=coef0, tol=tol, C=C).fit(genotype_train, genotypeOutput_train)
+	treatmentPredictor = svm.SVC(kernel=kernel, degree=degree, gamma=gamma, coef0=coef0, tol=tol, C=C).fit(treatment_train, treatmentOutput_train)
+	behaviorPredictor = svm.SVC(kernel=kernel, degree=degree, gamma=gamma, coef0=coef0, tol=tol, C=C).fit(behavior_train, behaviorOutput_train)
+
+	genotypeCorrect, genotypeIncorrect , genotypeFalsePositive, genotypeFalseNegative, totalGenotype = 0,0,0,0,0
+	for x, y in zip(genotype_test, genotypeOutput_test):
+		predicted = genotypePredictor.predict(x)[0]
+		if predicted == y:
+			genotypeCorrect += 1
 		else:
-			genotypeFalseNegative += 1
-	totalGenotype += 1
+			genotypeIncorrect += 1
+			if predicted == 1:
+				genotypeFalsePositive += 1
+			else:
+				genotypeFalseNegative += 1
+		totalGenotype += 1
 
-print '\nGenotype Analysis : \n'
-print 'Correct : {0:.2f} ({1:.2f} %)\nIncorrect : {2:.2f} ({3:.2f} %)\nFalse Positive : {4:.2f} ({5:.2f} %)\nFalse Negative : {6:.2f} ({7:.2f} %)'.format(genotypeCorrect, 100.0 * float(genotypeCorrect) / float(totalGenotype), genotypeIncorrect, 100.0 * float(genotypeIncorrect) / float(totalGenotype), genotypeFalsePositive, 100.0 * float(genotypeFalsePositive) / float(totalGenotype), genotypeFalseNegative, 100.0 * float(genotypeFalseNegative) / float(totalGenotype))
+	print '\nGenotype Analysis : \n'
+	print 'Correct : {0:.2f} ({1:.2f} %)\nIncorrect : {2:.2f} ({3:.2f} %)\nFalse Positive : {4:.2f} ({5:.2f} %)\nFalse Negative : {6:.2f} ({7:.2f} %)\n'.format(genotypeCorrect, 100.0 * float(genotypeCorrect) / float(totalGenotype), genotypeIncorrect, 100.0 * float(genotypeIncorrect) / float(totalGenotype), genotypeFalsePositive, 100.0 * float(genotypeFalsePositive) / float(totalGenotype), genotypeFalseNegative, 100.0 * float(genotypeFalseNegative) / float(totalGenotype))
+
+
+	treatmentCorrect, treatmentIncorrect , treatmentFalsePositive, treatmentFalseNegative, totalTreatment = 0,0,0,0,0
+	for x, y in zip(treatment_test, treatmentOutput_test):
+		predicted = treatmentPredictor.predict(x)[0]
+		if predicted == y:
+			treatmentCorrect += 1
+		else:
+			treatmentIncorrect += 1
+			if predicted == 1:
+				treatmentFalsePositive += 1
+			else:
+				treatmentFalseNegative += 1
+		totalTreatment += 1
+
+	print '\nTreatment Analysis : \n'
+	print 'Correct : {0:.2f} ({1:.2f} %)\nIncorrect : {2:.2f} ({3:.2f} %)\nFalse Positive : {4:.2f} ({5:.2f} %)\nFalse Negative : {6:.2f} ({7:.2f} %)\n'.format(treatmentCorrect, 100.0 * float(treatmentCorrect) / float(totalTreatment), treatmentIncorrect, 100.0 * float(treatmentIncorrect) / float(totalTreatment), treatmentFalsePositive, 100.0 * float(treatmentFalsePositive) / float(totalTreatment), treatmentFalseNegative, 100.0 * float(treatmentFalseNegative) / float(totalTreatment))
+
+	behaviorCorrect, behaviorIncorrect , behaviorFalsePositive, behaviorFalseNegative, totalBehavior = 0,0,0,0,0
+	for x, y in zip(behavior_test, behaviorOutput_test):
+		predicted = behaviorPredictor.predict(x)[0]
+		if predicted == y:
+			behaviorCorrect += 1
+		else:
+			behaviorIncorrect += 1
+			if predicted == 1:
+				behaviorFalsePositive += 1
+			else:
+				behaviorFalseNegative += 1
+		totalBehavior += 1
+
+	print '\nBehavior Analysis : \n'
+	print 'Correct : {0:.2f} ({1:.2f} %)\nIncorrect : {2:.2f} ({3:.2f} %)\nFalse Positive : {4:.2f} ({5:.2f} %)\nFalse Negative : {6:.2f} ({7:.2f} %)\n'.format(behaviorCorrect, 100.0 * float(behaviorCorrect) / float(totalBehavior), behaviorIncorrect, 100.0 * float(behaviorIncorrect) / float(totalBehavior), behaviorFalsePositive, 100.0 * float(behaviorFalsePositive) / float(totalBehavior), behaviorFalseNegative, 100.0 * float(behaviorFalseNegative) / float(totalBehavior))
+
+
 
 
 #This code was created and edited by Shelby Vanhooser
